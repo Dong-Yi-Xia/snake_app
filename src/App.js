18d5e22cc2cc9296c6dbc915cc9abb4dd1b83198
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-05-26 15:34:42 -0400

    added speed change

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Snake from './snake'
 import Food from './Food'
 
 
+
+//get mulitple of 2 from 2 to 98
 const getRandomCoordinates = () => {
   let min = 1
   let max = 98
@@ -40,7 +42,6 @@ class App extends Component {
   }
 
  
-
   componentDidUpdate(){
     this.checkIfOutOfBound()
     this.checkIfCollapsed() 
@@ -49,7 +50,7 @@ class App extends Component {
 
 
   // shouldComponentUpdate(nextProps, nextState) {
-  //   return this.state.pause !== nextState.pause
+  //   return this.state.speed !== nextState.speed
   // }
 
 
@@ -58,23 +59,47 @@ class App extends Component {
     e = e || window.event
 
     switch (e.keyCode){
-      case 37:
+      //left key
+      case 37: 
+        //when going left, cannot go right 
         if(this.state.direction === "RIGHT") break
+        if(this.state.direction === "LEFT") {
+          this.speedChange()
+          break
+        }
         this.setState({direction: 'LEFT'})
         break
-        
+      
+      //up key  
       case 38:
+        //when going up, cannot go down 
         if(this.state.direction === "DOWN") break
+        if(this.state.direction === "UP") {
+          this.speedChange()
+          break
+        }
         this.setState({direction: 'UP'})
         break
-        
+      
+      //right key  
       case 39:
+        //when going right, cannot go left 
         if(this.state.direction === "LEFT") break
+        if(this.state.direction === "RIGHT") {
+          this.speedChange()
+          break
+        }
         this.setState({direction: 'RIGHT'})
         break
-        
+       
+      //down key  
       case 40:
+        //when going down, cannot go up 
         if(this.state.direction === "UP") break
+        if(this.state.direction === "DOWN") {
+          this.speedChange()
+          break
+        }
         this.setState({direction: 'DOWN'})
         break
         
@@ -87,6 +112,7 @@ class App extends Component {
   movement = () => {
     let dots = [...this.state.snakeDots]
     let head = dots[dots.length - 1]
+    //head[X,Y] position
     switch (this.state.direction){
       case 'RIGHT':
         head = [head[0]+2, head[1]]
@@ -103,7 +129,7 @@ class App extends Component {
       default:
         break
     }
-
+    //add a new head and remove the tail. Give the effect of it is moving.
     dots.push(head)
     dots.shift()
     this.setState({
@@ -112,6 +138,7 @@ class App extends Component {
   }
 
 
+  //check it the snake is within bound
   checkIfOutOfBound = () => {
     let head = this.state.snakeDots[this.state.snakeDots.length -1]
     if(head[0] >= 100 || head[1] >= 100 || head[0] < 0 || head[1] < 0 ){
@@ -119,8 +146,11 @@ class App extends Component {
     }
   }
 
+  //check if the snake crashed into itself
   checkIfCollapsed=()=>{
     let snake = [...this.state.snakeDots]
+    //pop off the head and compare it to the body. 
+    //If you don't remove the head, it will always be true, when comparing head to head + body
     let head = snake.pop()
     snake.forEach(dot => {
       if(head[0] === dot[0] && head[1] === dot[1]){
@@ -129,6 +159,7 @@ class App extends Component {
     })
   }
 
+  //check if the head meets the food 
   checkIfEat=()=>{
     let head = this.state.snakeDots[this.state.snakeDots.length -1]
     let food = this.state.foodDot
@@ -143,8 +174,30 @@ class App extends Component {
 
   enlargeSnake=()=>{
     let newSnake = [...this.state.snakeDots]
+    //when the snake move it always add a new head position and remove the tail
+    //add a empty [] to the tail, then removing a empty gives the effect that its increasing in size.
     newSnake.unshift([])
     this.setState({snakeDots: newSnake})
+  }
+
+
+  speedChange=()=>{
+    this.setState({speed: 40}) 
+    this.updateGame()
+
+    //when key is release, reset speed back to default 
+    document.onkeyup =(e)=> {
+      this.setState({speed: 200})
+      this.updateGame()
+    }
+  }
+
+
+  updateGame=()=>{
+    if(!this.state.pause){
+      clearInterval(this.interveal) 
+      this.interveal = setInterval(this.movement, this.state.speed)
+    }
   }
 
 
@@ -164,6 +217,10 @@ class App extends Component {
     }))
   }
 
+
+  //using setInterval to repeat the function, in MS,
+  //need to use pass by reference to clearInterval
+  //DO NOT pass by value, clearInterval(setInterval(this.movement, this.state.speed))
   handlePlay=()=>{
     this.interveal = setInterval(this.movement, this.state.speed)
     this.setState(prevState => ({
@@ -175,7 +232,7 @@ class App extends Component {
 
 
   render(){
-    console.log("hi", this.state.pause)
+    console.log("hi", this.state.pause, this.state.speed)
 
     return (
       <div>
